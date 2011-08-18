30c7dbc6c77224e7c8703f89e365e4b629077f77
James Cox <james@imaj.es>
2011-08-18 12:20:25 -0400

    add a couple comments to make this clearer.

--- src/http_prebind.erl
+++ src/http_prebind.erl
@@ -17,20 +17,31 @@ process(_LocalPath, _Request) ->
   {403, [], "Forbidden"}.
 
 bind(Login, IP) ->
+  %% the Rid is the request id, and it starts with getting a random number from a string
   Rid = list_to_integer(randoms:get_string()),
+  %% gotta increment it for each message.
   Rid1 = integer_to_list(Rid + 1),
   {xmlelement, "body", Attrs1, _} = process_request("<body rid='"++Rid1++"' xmlns='http://jabber.org/protocol/httpbind' to='EJABBERD_DOMAIN' xml:lang='en' wait='60' hold='1' window='5' content='text/xml; charset=utf-8' ver='1.6' xmpp:version='1.0' xmlns:xmpp='urn:xmpp:xbosh'/>", IP),
+
+  # gets the Session ID and Auth Id from the auth request
   {value, {_, Sid}} = lists:keysearch("sid", 1, Attrs1),
   {value, {_, AuthID}} = lists:keysearch("authid", 1, Attrs1),
+
+  # this is the base64 auth sent to ejabberd
   Rid2 = integer_to_list(Rid + 2),
   Auth = base64:encode_to_string(AuthID++[0]++Login++[0]++"AUTH_PASSWORD"),
   process_request("<body rid='"++Rid2++"' xmlns='http://jabber.org/protocol/httpbind' sid='"++Sid++"'><auth xmlns='urn:ietf:params:xml:ns:xmpp-sasl' mechanism='PLAIN'>"++Auth++"</auth></body>", IP),
+
   Rid3 = integer_to_list(Rid + 3),
   process_request("<body rid='"++Rid3++"' xmlns='http://jabber.org/protocol/httpbind' sid='"++Sid++"' to='EJABBERD_DOMAIN' xml:lang='en' xmpp:restart='true' xmlns:xmpp='urn:xmpp:xbosh'/>", IP),
+
   Rid4 = integer_to_list(Rid + 4),
   {_,_,_,[{_,_,_,[{_,_,_,[{_,_,_,[{_,SJID}]}]}]}]} = process_request("<body rid='"++Rid4++"' xmlns='http://jabber.org/protocol/httpbind' sid='"++Sid++"'><iq type='set'><bind xmlns='urn:ietf:params:xml:ns:xmpp-bind'/></iq></body>", IP),
+
   Rid5 = integer_to_list(Rid + 5),
   process_request("<body rid='"++Rid5++"' xmlns='http://jabber.org/protocol/httpbind' sid='"++Sid++"'><iq type='set'><session xmlns='urn:ietf:params:xml:ns:xmpp-session'/></iq></body>", IP),
+
+  %% here's the return sent back over http.
   binary_to_list(SJID) ++ "\n" ++ Sid ++ "\n" ++ integer_to_list(Rid + 6).
 
 process_request(Request, IP) ->
